1706e032cff2dd3770580de183333d6a5e64d5b3
league <league@imac-16.attlocal.net>
2022-10-14 18:27:09 -0700

    esdr

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -17,7 +17,21 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if(randomNumber == 0) {
+			System.out.println("Your Cool");
+		}
+		else if(randomNumber == 1) {
+			System.out.println("Your Awesome");
+		}
+		else if(randomNumber == 2) {
+			System.out.println("Your Hot");
+		}
+		else if(randomNumber == 3) {
+			System.out.println("Your Smart");
+		}
+		else if(randomNumber == 4) {
+			System.out.println("Your Funny");
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,10 +6,28 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
-
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("What is a question for the 8 ball to answer");
+		Random ran = new Random();
+		int num = ran.nextInt(4);
+		if(num == 0) {
+			System.out.println("Yes");
+		}
+		else if(num == 1) {
+			System.out.println("No");
+		}
+		else if(num == 2) {
+			System.out.println("Maybe you should as Google");
+		}
+		else if(num == 3) {
+			System.out.println("Try Again");
+		}
+	}
 	// 2. Get the user to enter a question for the 8 ball to answer
 	
 
